a414fe2c45b0ac48837b41ec89c3c53bcf3b1634
isaac-collins <71849556+isaac-collins@users.noreply.github.com>
2021-10-29 15:23:58 -0700

    adding db support

---
+++ db-api/app.py
@@ -0,0 +1,24 @@
+import model
+import os, datetime
+from flask import Flask
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+DB_HOST = os.getenv('DB_HOST')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://{0}:{1}@{2}/images".format(DB_USER,DB_PASSWORD,DB_HOST)
+api = Api(app)
+
+db = SQLAlchemy(app)
+marsh = Marshmallow(app)
+
+image_schema = ImageSchema()
+images_schema = ImageSchema(many=True)
+
+api.add_resource(ImageResources, "/images")
+api.add_resource(ImageResource,'/images/<int:image_id>')
