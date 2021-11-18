658ed05ea835697283367192dc04143e27a44832
isaac-collins <71849556+isaac-collins@users.noreply.github.com>
2021-11-17 23:55:36 -0800

    clean

--- backend/app.py
+++ backend/app.py
@@ -3,37 +3,27 @@ from flask import Flask, render_template
 from flask import request, jsonify
 
 db_api = "http://db-api-svc/"
-
 app = Flask(__name__)
 
-def watermark_image(
-    b64_image,
-    text
-    ):
-    
+def watermark_image(b64_image, text):
     img_bytes = base64.b64decode(b64_image)
     image_array = numpy.frombuffer(img_bytes,dtype=numpy.uint8)
     img = cv2.imdecode(image_array,flags=cv2.IMREAD_COLOR)
-
     font = cv2.FONT_HERSHEY_COMPLEX
-
     org = (00,100)
-
     color = (0,0,255)
-
     img = cv2.putText(img, text, org, font,1,color,2,cv2.LINE_AA,False)
-
     retval, buffer = cv2.imencode('.jpg', img)
     encoded_image = base64.b64encode(buffer)
     encoded_image = encoded_image.decode("utf-8")
-    print(encoded_image)
+
     return encoded_image
 
 @app.route('/backend', methods=['POST','GET'])
 def index():
-    print(request.json)
     transformed_image = watermark_image(request.json['image'],request.json['text'])
-    resp = {
+
+    response = {
         'transformed_image': transformed_image
     }
     
@@ -41,9 +31,7 @@ def index():
         db_api + "images",
         json = {'transformed_image': transformed_image},
     )
-    return jsonify(resp)
-@app.route('/test')
-def test_route():
-    return "OK"
+    return jsonify(response)
+
 
 
--- db-api/app.py
+++ db-api/app.py
@@ -4,11 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from marshmallow import *
 from flask_restful import Resource, Api
 
-
 DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{0}:{1}@{2}/images".format(DB_USER,DB_PASSWORD,DB_HOST)
@@ -16,30 +14,36 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{0}:{1}@{2}/images".for
 api = Api(app)
 db = SQLAlchemy(app)
 
-
+#
+#Image object model
+#
 class Images(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.String(30))
-    data = db.Column(db.BINARY) #convert to BLOB
+    data = db.Column(db.BINARY) 
 
+#
+#Marsh schema
+#
 class ImageSchema(Schema):
-
     id = fields.Integer()
     timestamp = fields.String()
-    data = fields.Method("decode_image",deserialize="encode_image")
+    data = fields.Method("encode_image", deserialize="decode_image")
 
-    def decode_image(self, obj):
-        b64_image = base64.b64encode(obj.data)
-        return b64_image.decode("utf-8")
-
+    #convert bytes from db to b64 str
     def encode_image(self, obj):
+        return base64.b64encode(obj.data).decode("utf-8")
+
+    #convert b64 string from client to bytes 
+    def decode_image(self, obj):
         return base64.b64decode(obj.data)
-
-
 
 image_schema = ImageSchema()
 images_schema = ImageSchema(many=True)
 
+#
+#Flask_Restful resources
+#
 class ImageResources(Resource):
     def get(self):
         images = Images_schema.query.all()
@@ -48,7 +52,7 @@ class ImageResources(Resource):
     def post(self):
         image = Images(
             timestamp = datetime.datetime.now(),
-            data = base64.b64decode(request.json["transformed_image"])
+            data = request.json["transformed_image"]
         )
         db.session.add(image)
         db.session.commit()
@@ -60,6 +64,6 @@ class ImageResource(Resource):
         return image_schema.dump(image)
 
 api.add_resource(ImageResources, "/images")
-api.add_resource(ImageResource,'/images/<int:image_id>')
+api.add_resource(ImageResource, "/images/<int:image_id>")
 
 
