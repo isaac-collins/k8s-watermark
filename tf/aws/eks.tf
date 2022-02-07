880007e61629a8c389f493f6787adbe9418448b1
isaac-collins <71849556+isaac-collins@users.noreply.github.com>
2022-02-07 15:10:11 -0800

    Update eks.tf

--- tf/aws/eks.tf
+++ tf/aws/eks.tf
@@ -11,23 +11,14 @@ module "eks" {
     root_volume_type = "gp2"
   }
 
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-    },
-  ]
-}
+  node_groups = {
+    one = {
+      desired_capacity = 1
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type = "t2.medium"
+    }
+  }
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
