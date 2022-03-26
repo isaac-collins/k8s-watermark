7a5f59fab08bdc2d7d48e6bd5db3bbf113d63c07
isaac-collins <71849556+isaac-collins@users.noreply.github.com>
2022-03-26 16:09:57 -0700

    adding unit test

---
+++ backend/.test/test_backend.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('../backend')
+from backend.app import *
+
+
+#
+# unit test to verify watermark_image() converts image 
+#
+def test_watermark_image():
+    test_input = open('test_input','r').read()
+    test_output = open('test_output','r').read()
+
+    assert watermark_image(test_input, "test") == test_output 
